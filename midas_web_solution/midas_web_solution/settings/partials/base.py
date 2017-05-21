453be1e0f8ec1d47084ceec1ad4f1c021179b5e9
jupiny <tmdghks584@gmail.com>
2017-05-21 16:13:51 +0900

    home 뷰 함수 생성 및 템플릿, url 연결

--- midas_web_solution/midas_web_solution/settings/partials/base.py
+++ midas_web_solution/midas_web_solution/settings/partials/base.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'midas_web_solution.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
