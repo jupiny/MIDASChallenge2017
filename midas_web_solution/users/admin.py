d1c270fabd444fd602fa8b68b908dcec76cd24d2
znxkznxk1030 <znxkznxk1030@gmail.com>
2017-05-28 00:55:50 +0900

    [ysKim] food 단에서 user 관리자 검사

--- midas_web_solution/users/admin.py
+++ midas_web_solution/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if request.user.is_superuser:
+            obj.is_staff = True
+            obj.save()
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
